fdc098579de74f70c94bb21ec9ac16e54df5b827
Nikhil Pallamreddy <nikhil.pallamreddy@mavq.com>
2021-07-20 17:57:14 +0530

    Add indexing in mongo

--- qwiz-backend/src/main/java/com/quantumdisruption/qwiz/QWiz/domain/Quiz.java
+++ qwiz-backend/src/main/java/com/quantumdisruption/qwiz/QWiz/domain/Quiz.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.validation.constraints.NotBlank;
@@ -29,6 +30,8 @@ public class Quiz {
     @Id
     private String id;
 
+    @NotBlank
+    @Indexed(unique = true)
     private String name;
     private Date date;
     private List<Integer> intro;
