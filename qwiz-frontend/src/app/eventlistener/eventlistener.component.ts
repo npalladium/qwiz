974a9aa8806691ffd007899492e80aced5349e04
Nikhil Pallamreddy <nikhil.pallamreddy@mavq.com>
2021-07-20 12:34:09 +0530

    Display attempts as a formated string on dashboard
    Changes:
    - Add sidenav component to display answers
    - Format with answers filter

--- qwiz-frontend/src/app/eventlistener/eventlistener.component.ts
+++ qwiz-frontend/src/app/eventlistener/eventlistener.component.ts
@@ -5,18 +5,17 @@ import {Subject, Observable, Observer }  from 'rxjs';
 
 @Component({
   selector: 'app-eventlistener',
-  // templateUrl: './eventlistener.component.html',
-  template: `<h1>My second Angular 2 App</h1>
+  template: `
     <ul>
         <li *ngFor="let s of someStrings">
-           a string: {{ s }}
+           a string: {{ s | answers }}
         </li>
     </ul>
     `,
   styleUrls: ['./eventlistener.component.css']
 })
 export class EventlistenerComponent implements OnInit {
-  someStrings:string[] = [];
+  someStrings:any[] = [];
   subscription: any;
 
   constructor(private zone: NgZone) {}
@@ -27,7 +26,7 @@ export class EventlistenerComponent implements OnInit {
       const eventSource = new EventSource('http://localhost:8080/stream-pounces', {withCredentials: true});
       eventSource.onmessage = x => observer.next(x.data);
       eventSource.onerror = x => observer.error(x);
-      eventSource.addEventListener('pounce', (message: any) => this.someStrings.push(JSON.parse(message.data).message));
+      eventSource.addEventListener('pounce', (message: any) => this.someStrings.push(JSON.parse(message.data)));
 
     });
 
