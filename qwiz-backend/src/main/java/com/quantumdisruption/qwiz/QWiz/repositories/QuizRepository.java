4f88dd0564965a7bce21f9f18220c9dfce2b5d34
Nikhil Pallamreddy <nikhil.pallamreddy@mavq.com>
2021-07-20 12:43:25 +0530

    Make quizzes endpoint cross-origin
    Because:
    - It needs to be accessible to Angular when running locally
    
    TODO:
    - Make the permissions tighter
    - Switch it off in prod

--- qwiz-backend/src/main/java/com/quantumdisruption/qwiz/QWiz/repositories/QuizRepository.java
+++ qwiz-backend/src/main/java/com/quantumdisruption/qwiz/QWiz/repositories/QuizRepository.java
@@ -3,7 +3,9 @@ package com.quantumdisruption.qwiz.QWiz.repositories;
 import com.quantumdisruption.qwiz.QWiz.domain.Quiz;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "quizzes", path = "quizzes")
 public interface QuizRepository extends MongoRepository<Quiz, String> {
 }
