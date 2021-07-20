e1ef9cea54c6d0d6d33f450353ebd29164ba4dad
Nikhil Pallamreddy <nikhil.pallamreddy@mavq.com>
2021-07-20 07:34:44 +0530

    Make angular work in docker

--- qwiz-frontend/src/app/services/file-upload.service.ts
+++ qwiz-frontend/src/app/services/file-upload.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpRequest, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FileUploadService {
-  private baseUrl = 'http://localhost:8080';
+  private baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
